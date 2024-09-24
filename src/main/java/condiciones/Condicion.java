7b93bbf456b050bc7be1d5d7f1872d3bc9c8a175
Emiliana Girardi <girardiemiliana@gmail.com>
2024-09-24 14:49:41 -0300

    Se agregan condiciones y ordenamientos faltantes

--- src/main/java/condiciones/Condicion.java
+++ src/main/java/condiciones/Condicion.java
@@ -10,7 +10,7 @@ public interface Condicion {
         private String valorLU;
         @Override
         public String getCondicion(){
-            String condicion = "WHERE e.libretaUniversitaria = ".concat('"'+ valorLU + '"');
+            String condicion = "WHERE e.libreta_universitaria = " + valorLU;
             return condicion;
         }
     }
@@ -20,7 +20,7 @@ public interface Condicion {
         private char genero;
         @Override
         public String getCondicion(){
-            String condicion = "WHERE e.genero = ".concat('"'+ String.valueOf(genero) + '"');
+            String condicion = "WHERE e.genero = " + genero;
             return condicion;
         }
     }
@@ -33,30 +33,40 @@ public interface Condicion {
         }
     }
 
-    @AllArgsConstructor
-    public class CondicionInsc implements Condicion{
-        @Override
-        public String getCondicion(){
-            return "";
-        }
-    }
 
     @AllArgsConstructor
-    public class CondicionEstXCarreraYCiudad implements Condicion {
+    public class CondicionEstPorCarreraYCiudad implements Condicion{
         private int idCarrera;
         private String ciudad;
+
         @Override
-        public String getCondicion(){
-            String query = "WHERE e.ciudad =";
-            return null;
+        public String getCondicion() {
+            return "WHERE i.id_carrera = " + idCarrera +
+                    "and i.dni_estudiante in " +
+                    "(SELECT e.dni_estudiante from Estudiate e where e.ciudad = " + ciudad;
         }
+
+        /* en InscripcionRepository:
+
+        public List<Estudiante> getEstudiante(Condicion c, Ordenamiento o) {
+            String query = "SELECT i.dni_estudiante FROM Inscripcion i".concat(c.getCondicion()).concat(ord.getOrdenamiento());
+            TypedQuery<Inscripcion> q = em.createQuery(query, Estudiante.class);
+            List<String> result = q.getResultList();
+            List<Estudiante> estudiantes = new List<Estudiante>();
+            for each (dni in result){
+                estudiantes.add(EstudianteRepository.getEstudianteById(dni);
+            }
+            return estudiantes;
+        }
+         */
+
     }
 
-    @AllArgsConstructor
-    public class CondicionCarr implements Condicion{
+    public class CondicionEstPorCiudad implements Condicion{
+        private String ciudad;
         @Override
-        public String getCondicion(){
-            return "";
+        public String getCondicion() {
+            return "WHERE e.ciudad = " + ciudad;
         }
     }
 }
